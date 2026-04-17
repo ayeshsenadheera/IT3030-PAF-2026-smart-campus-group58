46c510e520b241a33d092baef7f3fb6688a39efa
IT23836518 <it23836518@my.sliit.lk>
2026-04-17 22:24:07 +0530

    Add Resource Type

--- backend/src/main/java/com/smartcampus/dto/request/ResourceRequest.java
+++ backend/src/main/java/com/smartcampus/dto/request/ResourceRequest.java
@@ -1,7 +1,12 @@
 package com.smartcampus.dto.request;
 
+import com.smartcampus.enums.ResourceType;
+
 public class ResourceRequest {
     
     @NotBlank(message = "Resource name is required")
     private String name;
+
+    @NotNull(message = "Resource type is required")
+    private ResourceType type;
 }
